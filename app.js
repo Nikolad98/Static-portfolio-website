b0350d74db57a9ac10520e2714ae6692b6955d13
Nikola <nikola.dreznjak98@gmail.com>
2021-01-19 07:57:12 -0800

    mobile done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger')
 const navbar = document.querySelector('.navbar')
 const navbarButtons = document.querySelectorAll('.navbar ul li a')
+const header = document.querySelector('header')
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('hidden')
@@ -9,4 +10,13 @@ hamburger.addEventListener('click', () => {
             navbar.classList.add('hidden')
         })
     })
+})
+
+document.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY;
+    if (scrollPosition > 350){
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.4)"
+    }else{
+        header.style.backgroundColor = "transparent"
+    }
 })
